6b547446b1cb70bc71a789b509c5ba8ea2c22b70
DanDChoi <showbegin622@gmail.com>
2021-11-26 18:29:10 +0900

    1126-4

---
+++ Java/Mission/15_Multichat/Option/MOption.java
@@ -0,0 +1,57 @@
+package setting;
+
+import java.io.IOException;
+import java.util.Vector;
+
+public class MOption {
+
+    MOption(){
+
+    }
+
+    void opt() {
+        try {
+            String str = null;
+            switch (str) {
+            case "list":
+                list();
+                break;
+            case "kick":
+                kick();
+                break;
+            default:
+                System.out.println("올바른 option을 선택해주세요");
+                opt();
+            }
+
+        } catch (IOException ie) {
+            opt();
+        }
+    }
+    void list(){
+        pln(Vector)
+    }
+    void kick(){
+        String str = inputTarget("");
+        if (str == null){
+            pln("");
+            kick();
+        } else {
+            for (OneCliMod oneCliMod : this.v){
+                if (str.equals(oneCliMod.chatId)){
+                    this.v.remove(oneCliMod);
+                    MOption.on_off = 0;
+                    return;
+                }
+            }
+        }
+    }
+
+    void p(String str){
+        System.out.print(str);
+    }
+    void pln(String str){
+        System.out.println(str);
+    }
+
+}
