b58b7f20688371dae446aa37c6e5b065abd0edb4
DanDChoi <showbegin622@gmail.com>
2022-01-05 11:02:59 +0900

    0104-1

--- Java/JavaAdvanced/jdbc/B.java
+++ Java/JavaAdvanced/jdbc/B.java
@@ -10,7 +10,7 @@ class B
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			con = DriverManager.getConnection(url, "scott", "tiger");
 			stmt = con.createStatement();
-			pln("stmt 생성 단계까지 성공");
+			//pln("stmt 생성 단계까지 성공");
 		}catch(ClassNotFoundException cnfe){
 			pln("드라이버로딩 실패(클래스를 못 찾음)");
 		}catch(SQLException se){
@@ -18,7 +18,10 @@ class B
 		}
 
 		//createT();
-        //createTnoExist(); //나중에.. 미션!
+
+        createTnoExist(); //미션! 
+		//힌트: sql = select TABLE_NAME from user_tables where TABLE_NAME='"+tname+"';
+
 		//dropT();
 
 		//insertD(10, "홍길동");
@@ -27,7 +30,11 @@ class B
 
 		//updateD(20, "신병철");
 
-		deleteD(10); // 쉬는 시간 갖으면서.. 각자 시도해보세요
+		//deleteD(10); // 쉬는 시간 갖으면서.. 각자 시도해보세요
+
+		selectD();
+
+		closeAll();
 	}
 
     //DDL 
@@ -39,6 +46,19 @@ class B
 			pln(tname+"테이블 생성 성공");
 		}catch(SQLException se){
             pln(tname+"테이블 생성 실패: " + se);
+		}
+	}
+	void createTnoExist(){
+		String sql = "select TABLE_NAME from user_tables where TABLE_NAME='"+tname+"'";
+		ResultSet rs = null;
+		try{
+			rs = stmt.executeQuery(sql);
+			if(!rs.next()) createT();
+		}catch(SQLException se){
+		}finally{
+			try{
+				rs.close();
+			}catch(SQLException se){}
 		}
 	}
 	void dropT(){
@@ -81,7 +101,53 @@ class B
 		}
 	}
 	void deleteD(int no){ //구현!!
+        String sql = "delete from "+tname+" where NO="+no;
+		try{
+			int i = stmt.executeUpdate(sql);
+			if(i>0){
+				pln(i+"개의 row 삭제 성공");
+			}else{
+			    pln("삭제되지 않음");
+			}
+		}catch(SQLException se){
+			pln("삭제 실패: " + se);
+		} 
+	}
 
+	//DQL
+	void selectD(){
+		String sql = "select * from "+tname+" order by RDATE desc";
+		ResultSet rs = null;
+		try{
+			rs = stmt.executeQuery(sql);
+			int cnt = 0;
+			pln("NO\tNAME\tRDATE");
+			pln("--------------------------");
+			while(rs.next()){
+				int no = rs.getInt("NO"); 
+				String name = rs.getString("NAME");
+				Date rdate = rs.getDate("RDATE");
+				pln(no+"\t"+name+"\t"+rdate);
+
+				cnt++;
+			}
+			pln("--------------------------");
+			pln("총 "+cnt+"개 데이터 검색됨");
+		}catch(SQLException se){
+            pln("selectD() 예외: " + se);  
+		}finally{
+            try{
+				rs.close();
+			}catch(SQLException se){}
+		}
+	}
+    
+	void closeAll(){
+		try{
+		    stmt.close();
+			con.close();
+		}catch(SQLException se){
+		}
 	}
 	void pln(String str){
 		System.out.println(str);
