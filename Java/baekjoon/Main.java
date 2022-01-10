9a53b9da0480583bab0c7f1ff665f554c58a97e6
DanDChoi <showbegin622@gmail.com>
2022-01-10 22:21:52 +0900

    0110-1

--- Java/baekjoon/Main.java
+++ Java/baekjoon/Main.java
@@ -2,9 +2,14 @@ import java.util.*;
 
 class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int i;
-        int j;
-        for(int i>=1; i=9; i++)
-            System.out.println(i+"*"+)
+        i = sc.nextInt();
+
+        for(int j=1; j<=i; j++){
+            int k;
+            k = (0+1)*i;
+            System.out.println(k);
+        }
     }
 }
