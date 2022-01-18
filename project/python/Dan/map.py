76822b0f1c2dbdfdfb6860fd79c7bd7e5c0c462f
DanDChoi <showbegin622@gmail.com>
2022-01-18 12:22:28 +0900

    0117-1

--- project/python/Dan/map.py
+++ project/python/Dan/map.py
@@ -1,11 +1,13 @@
+#뱀이 이동하는 맵을 생성하고, 게임 진행을 담당
+
 from platform import release
 from snake import *
-from PyQt5.QtCore import *
+from PyQt5.QtCore import *   #PyQt5를 이용한 GUI처리
 from PyQt5.QtGui import *
-from threading import Thread, Lock
-import time
+from threading import Thread, Lock 
+import time #thread의 sleep()지연을 위해 사용
 import pygame
-from random import randint
+from random import randint #맵에 랜덤하게 먹이를 만들어내기 위해 사용
 
 class CMap:
     def __init__(self, parent):
@@ -23,7 +25,7 @@ class CMap:
         self.parent = parent #부모 윈도우 저장
         self.outrect = parent.rect() #부모 윈도우 크기 저장
         gap = 20 # 내부 맵 여백 조정
-        self.inrect = self.outrect.adjusted(gap, gap, -gap, -gap)
+        self.inrect = self.outrect.adjusted(gap, gap, -gap, -gap) #실제 맵(바둑판식맵)이 그려지는 사각형의 좌표를 저장
         
         
         pygame.init()
@@ -46,16 +48,15 @@ class CMap:
         topleft = QPoint(self.left, self.top)
         size = QSize(self.wsize, self.hsize)
         
-        for i in range(self.lines):
+        for i in range(self.lines): #위에 생성된 225개의 사각형 좌표를 저장하는 변수에 2중 반복문을 통해 실제 윈도우의 좌표를 저장
             for j in range(self.lines):
                 self.rect[i][j] = QRect(self.left+(j*self.wsize)
                                         , self.top+(i*self.hsize)
                                         , self.wsize
                                         , self.hsize)
-                self.rect[i][j].adjust(1, 1, -1, -1) #사각형 크기 줄이기
+                self.rect[i][j].adjust(2, 2, -2, -2) #사각형 크기 줄이기(뱀이 사각형에 꽉 차지 않게)
                 
-    def reStart(self):
-        
+    def reStart(self):  # 게임 종료시 재시작을 담당, 쓰레드 삭제, 재시작 및 각종 변수를 초기화
         del(self.thread)
         self.thread = Thread(target = self.playgame)
         self.bRun = True
@@ -68,31 +69,33 @@ class CMap:
         del(self.food)
         self.food = CNode(-1, -1)
         
-    def draw(self, qp): #맵그리기
+    def draw(self, qp): #그림을 그리는 역할 (사각형 맵, 뱀, 먹이등등), CMap의 생성자에서 전달인자로 넘겨받은 부모윈도우(QWidget)의 PaintEvent()함수에의해 호출
         for i in range(self.lines+1):
             #가로선
             qp.drawLine(self.left, self.top+(i*self.hsize), self.right, self.top+(i*self.hsize))
             #세로선
             qp.drawLine(self.left+(i*self.wsize), self.top, self.left+(i*self.wsize), self.bottom)
+            #Qpainter의 drawLine()함수는 시작점과 끝점 좌표를 전달인자로 입력하면 해당 점을 연결하는 선을 그림
             
         #뱀 그리기
         i = 0
         self.lock.acquire()
         for node in self.snake.node:
-            if i == 0:
+            if i == 0: #첫마디(인덱스:0)는 머리이므로 다른 색으로 칠함
                 qp.setBrush(QColor(33, 53, 194))
             else:
                 qp.setBrush(QColor(83, 103, 255, 128))
             if self.bRun:
-                qp.drawRect(self.rect[node.y][node.x])
+                qp.drawRect(self.rect[node.y][node.x]) #Qpainter의 drawRect()함수는 QRect클래스타입을 전달인자로 받아 사각형을 그리는 함수
             i += 1
         self.lock.release()
         self.lock.acquire()
+        #뱀 마디를 저장하는 snake.node 리스트는 별도의 thread에서 동시에 해당 리스트에 접근될 수 있으므로, 반드시 동기화처리가 필요
         
         #먹이그리기
         if self.food.x != -1 and self.food.y != -1:
             qp.setBrush(QColor(255, 0, 0))
-            qp.drawEllipse(self.rect[self.food.y][self.food.x])
+            qp.drawEllipse(self.rect[self.food.y][self.food.x]) #drawEllipse는 사각형 좌표에 내접하는 타원으로 모양을 그림
         self.lock.release()
         
         qp.drawText(self.outrect, Qt.AlignTop|Qt.AlignLeft, '점수:'+str(self.foodcnt))
@@ -102,18 +105,18 @@ class CMap:
             qp.setFont(QFont('맑은고딕', 20))
             qp.drawText(self.outrect, Qt.AlignCenter, '키보드 방향키를 누르면 시작')
             
-    def keydown(self, key):
+    def keydown(self, key): #QWdiget클래스의 키보드 눌러짐(keyPressEvent)이 감지되면 호출되는 함수
         #게임시작
         if (key == Qt.Key_Right or key == Qt.Key_Up or key == Qt.Key_Down) and self.bGame == False:
             self.bGame = True
             self.snake.changeDir(key)
-            self.thread.start()
-            self.bgm.play(-1)
+            self.thread.start() #쓰레드 실행, 게임시작
+            self.bgm.play(-1) # -1을 통해 노래 무한반복
         #게임진행중 키 변경
         if (key == Qt.Key_Left or key == Qt.Key_Right or key == Qt.Key_Up or key == Qt.Key_Down) and self.bGame == True:
             if self.bMove:
                 self.snake.changeDir(key)
-                self.bMove = False
+                self.bMove = False #한번 키보드의 방향이 변경되면 상태 플래그를 변경시켜, 뱀이 한번 이동한 후에만 키보드를 입력하도록 막아주는 역할 (버그방지)
     def makeFood(self):
         #먹이가 있으면 리턴
         if self.food.x != -1 and self.food.y != -1:
@@ -147,7 +150,7 @@ class CMap:
             return True
         else:
             return False
-    def isOut(self, head):
+    def isOut(self, head): #뱀 머리의 좌표가 0보다 작거나 15보다 크면 맵의 밖으로 나갔으므로 True를 리턴
         if head.x < 0 or head.x >= self.lines:
             return True
         elif head.y < 0 or head.y >= self.lines:
@@ -155,7 +158,7 @@ class CMap:
         else:
             return False
             
-    def playgame(self):
+    def playgame(self): #쓰레드에 의해 호출되는 함수, 무한루프로 구성/ 1.뱀이동 2.먹이생성 3.먹이먹음여부 4.마디추가를 수행하는 함수로 매 0.3초마다 반복호출
         while self.bRun:
             self.lock.acquire()
             #뱀 이동, 머리, 몸통 충돌시 종료 or 맵 나가면 종료
--- project/python/Dan/window.py
+++ project/python/Dan/window.py
@@ -1,8 +1,10 @@
+#프로그램 창을 생성하는 역할을 담당
+
 from map import *
 from PyQt5.QtWidgets import QWidget, QApplication, QMessageBox
 
-class CWidget(QWidget):
-    endSignal = pyqtSignal()
+class CWidget(QWidget): #Qwidget에서 상속받은 클래스
+    endSignal = pyqtSignal() #게임종료 시그널을 클래스변수로 생성 (객체변수X)
     #시그널 생성
     def __init__(self): 
         super().__init__()
@@ -16,19 +18,19 @@ class CWidget(QWidget):
         self.setWindowTitle('Snake Game')
         self.setFixedSize(self.rect().size())
         
-        self.map = CMap(self)
-        self.endSignal.connect(self.ExitGame)
+        self.map = CMap(self) #핵심클래스인 CMap 클래스 변수를 선언하는 코드
+        self.endSignal.connect(self.ExitGame) 
         
-    def paintEvent(self, e):
-        qp = QPainter()
+    def paintEvent(self, e): #QWidget클래스의 함수 오버라이딩
+        qp = QPainter() #QPainter 클래스의 변수인 qp를 선언, CMap클래스의 draw()함수로 전달인자를 넘김으로 그리기 권한을 위임하는 형식
         qp.begin(self)
         self.map.draw(qp)
         qp.end()
     
-    def keyPressEvent(self, e):
+    def keyPressEvent(self, e): #QWidget 클래스의 함수오버라이딩, 키보드를 누르면 그 처리를 CMap의 Keydown()함수를 호출해 위임
         self.map.keydown(e.key())
         
-    def ExitGame(self):
+    def ExitGame(self): #CMap에서 게임종료 조건이 충족되면 CWidget으로 보내는 시그널과 연결된 슬롯함수
         result = QMessageBox.information(self
                                          ,'게임종료'
                                          ,'다시시작: Yes, 종료: No'
