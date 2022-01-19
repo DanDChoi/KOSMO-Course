8c73e35b50621f14edef17f2ca30ff91ac9bc037
Dan Daehyun Choi <showbegin622@gmail.com>
2022-01-19 20:20:23 +0900

    0119-4

--- Java/paikjun/Main.java
+++ Java/paikjun/Main.java
@@ -1,11 +1,23 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 
-class Main{
-   public static void main(String[] args){
-       Scanner sc = new Scanner(System.in);
-       double a, b;
-       a = sc.nextInt();
-       b = sc.nextInt();
-       System.out.println(a / b);
-   }
+
+class Main {
+    BufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i;
+        i = sc.nextInt();
+
+        for(int l=1; l<=i; l++){
+            int j;
+            int k;
+            j = sc.nextInt();
+            k = sc.nextInt();
+            System.out.println(j+k);
+        }
+    }
+    BufferedReader.flush();
 }
