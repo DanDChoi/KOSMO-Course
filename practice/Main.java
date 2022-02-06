875607b7af19c949b2acfe934411b96266880c45
DanDChoi <showbegin622@gmail.com>
2022-02-06 15:03:21 +0900

    0206-2

---
+++ practice/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void practice(int j){
+        int i;
+        for (i=0; i < j; i++){
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args){
+       practice(6);
+    }
+}
