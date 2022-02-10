527b989ed21ab73b3a44bd7829f58b3741635a7d
DanDChoi <showbegin622@gmail.com>
2022-02-10 19:56:04 +0900

    0210-2

--- practice/Test.java
+++ practice/Test.java
@@ -2,15 +2,12 @@ import java.util.ArrayList;
 
 public class Test {
     public static void main(String[] args){
-    ArrayList<String> list = new ArrayList<String>();
-        list.add("one");
-        list.add("two");
-        list.add("three");
-    
-    for(int i = 0; i<list.size(); i++){
-        String value = list.get(i);
-        System.out.println(value);
-        }
+        String str = "AAA";
+        long l = 123;
+        Integer i = 1;
+
+        System.out.println(str.getClass().getName());
+        System.out.println(i.getClass().getName());
 
     }
 }
