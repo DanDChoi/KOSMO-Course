4037f8b40444eaf78b958fdb86156712528b75c7
Dan Daehyun Choi <showbegin622@gmail.com>
2022-03-02 23:50:55 +0900

    0302-1

--- Java/baekjoon/Main.java
+++ Java/baekjoon/Main.java
@@ -1,31 +1,4 @@
-/*
-import java.util.*;
 
-class Main {
-    public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int i = sc.nextInt(); 
-    int j = sc.nextInt(); 
-    int k = sc.nextInt(); 
-    sc.close();
-	
-	int r1, r2, r3;
-	if(i == j && j == k){
-		r1 = i*1000+10000;
-		System.out.println(r1);
-	}else if (i == j || i == k || j == k){
-		r2 = i*100 + 1000;
-		System.out.println(r2);
-	}else if(i != j || i != k || j != k){
-		int[] list = {i, j, k};
-		Arrays.sort(list);
-		int max = list[list.length -1];
-		r3 = max*100;
-		System.out.println(r3);
-	}
-	}
-}
-*/
 import java.util.Scanner;
 
 class Main {
